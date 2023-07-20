1761182deeb3cc735b574255e9f4896608995196
Renato Alpes <renatinhoas@hotmail.com>
2023-07-20 21:59:39 +0000

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,21 +1,30 @@
 -- pessoas
-INSERT INTO pessoa (cpf,nome,rua,numero,complemento,cep,idade) VALUES ('','','',,'','',);
-INSERT INTO pessoa (cpf,nome,rua,numero,complemento,cep,idade) VALUES ('','','',,'','',);
-INSERT INTO pessoa (cpf,nome,rua,numero,complemento,cep,idade) VALUES ('','','',,'','',);
-INSERT INTO pessoa (cpf,nome,rua,numero,complemento,cep,idade) VALUES ('','','',,'','',);
+INSERT INTO pessoa (cpf,nome,rua,numero,complemento,cep,idade) VALUES ('00698199049','','',,'','',);
+INSERT INTO pessoa (cpf,nome,rua,numero,complemento,cep,idade) VALUES ('68493475009','','',,'','',);
+INSERT INTO pessoa (cpf,nome,rua,numero,complemento,cep,idade) VALUES ('48762903004','Hackerman','rainbow road',3735928559,'x86 mov is turing complete','43805610',42);
+INSERT INTO pessoa (cpf,nome,rua,numero,complemento,cep,idade) VALUES ('20442555024','','',,'','',);
 INSERT INTO pessoa (cpf,nome,rua,numero,complemento,cep,idade) VALUES ('29909148030','Perry o ornitorrinco','maple drive',2308,'casinha no quintal','69309660',5);
 INSERT INTO pessoa (cpf,nome,rua,numero,complemento,cep,idade) VALUES ('29909148031','Agente P','maple drive',2308,'subsolo','69309660',5);
 INSERT INTO pessoa (cpf,nome,rua,numero,complemento,cep,idade) VALUES ('49804012014','Robert''); DROP TABLE students; -- ','sanitize',362,'your inputs','58058095',12);
-INSERT INTO pessoa (cpf,nome,rua,numero,complemento,cep,idade) VALUES ('','','',,'','',);
-INSERT INTO pessoa (cpf,nome,rua,numero,complemento,cep,idade) VALUES ('','','',,'','',);
-INSERT INTO pessoa (cpf,nome,rua,numero,complemento,cep,idade) VALUES ('','','',,'','',);
-INSERT INTO pessoa (cpf,nome,rua,numero,complemento,cep,idade) VALUES ('','','',,'','',);
-INSERT INTO pessoa (cpf,nome,rua,numero,complemento,cep,idade) VALUES ('','','',,'','',);
-INSERT INTO pessoa (cpf,nome,rua,numero,complemento,cep,idade) VALUES ('','','',,'','',);
-INSERT INTO pessoa (cpf,nome,rua,numero,complemento,cep,idade) VALUES ('','','',,'','',);
+INSERT INTO pessoa (cpf,nome,rua,numero,complemento,cep,idade) VALUES ('67575259090','','',,'','',);
+INSERT INTO pessoa (cpf,nome,rua,numero,complemento,cep,idade) VALUES ('18878090077','Alma Armas','va',11,'hall-A','68373150',29);
+INSERT INTO pessoa (cpf,nome,rua,numero,complemento,cep,idade) VALUES ('82322846090','Julianne Natalie Stingray','va',11,'hall-A','68373150',27);
+INSERT INTO pessoa (cpf,nome,rua,numero,complemento,cep,idade) VALUES ('39564815002','Anna Graem','va',11,'hall-A','68373150',21);
+INSERT INTO pessoa (cpf,nome,rua,numero,complemento,cep,idade) VALUES ('82523784041','Dana Zane','va',11,'hall-A','68373150',0);
+INSERT INTO pessoa (cpf,nome,rua,numero,complemento,cep,idade) VALUES ('32034627024','Gillian','va',11,'hall-A','68373150',0);
+INSERT INTO pessoa (cpf,nome,rua,numero,complemento,cep,idade) VALUES ('85132578004','Dorothy Haze','va',11,'hall-A','68373150',24);
 -- telefones
 -- funcionarios
+INSERT INTO funcionario (cpf,cargo,cpf_supervisor) VALUES (82523784041,'gerente',82523784041);
+INSERT INTO funcionario (cpf,cargo,cpf_supervisor) VALUES (32034627024,'supervisor',82523784041);
+INSERT INTO funcionario (cpf,cargo,cpf_supervisor) VALUES (82322846090,'supervisor',82523784041);
+INSERT INTO funcionario (cpf,cargo,cpf_supervisor) VALUES (18878090077,'atendente',82322846090);
+INSERT INTO funcionario (cpf,cargo,cpf_supervisor) VALUES (,'',);
 -- clientes
+INSERT INTO cliente (cpf,pontos) VALUES (39564815002,326);
+INSERT INTO cliente (cpf,pontos) VALUES (85132578004,426);
+INSERT INTO cliente (cpf,pontos) VALUES (,);
+INSERT INTO cliente (cpf,pontos) VALUES (,);
 -- produtos
 INSERT INTO produto (codigo,nome,lote) VALUES (60649, 'achocolatado', '453ytu2-243');
 INSERT INTO produto (codigo,nome,lote) VALUES (46494, 'loja de conveniencia', '7yw34yr873-4387');
@@ -44,7 +53,13 @@ INSERT INTO fornecedor (cnpj,nome_da_empresa) VALUES (13993089000186,'Toby Fox')
 INSERT INTO fornecedor (cnpj,nome_da_empresa) VALUES (97170639000157,'Evil Corp');
 INSERT INTO fornecedor (cnpj,nome_da_empresa) VALUES (76994489000126,'Zaibatsu Corp');
 INSERT INTO fornecedor (cnpj,nome_da_empresa) VALUES (96104203000105,'[object Object]');
+
 -- dependentes
+INSERT INTO dependente (cpf_empr,nome_completo) VALUES (82322846090,'Fore');
+INSERT INTO dependente (cpf_empr,nome_completo) VALUES (,'');
+INSERT INTO dependente (cpf_empr,nome_completo) VALUES (,'');
+INSERT INTO dependente (cpf_empr,nome_completo) VALUES (,'');
+
 -- produtos fornecidos
 INSERT INTO produtoFornecido (cnpj,codigo) VALUES (33855174000195,60649);
 INSERT INTO produtoFornecido (cnpj,codigo) VALUES (33855174000195,60554);
