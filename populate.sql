513e8c0ca20805ef71b0638053141d91828faee3
Renato Alpes <renatinhoas@hotmail.com>
2023-07-20 19:31:22 +0000

    produtos

--- populate.sql
+++ populate.sql
@@ -1,1 +1,29 @@
+-- pessoas
 
+-- telefones
+-- funcionarios
+-- clientes
+-- produtos
+INSERT INTO produto (codigo,nome,lote) VALUES (60649, 'achocolatado', '453ytu2-243');
+INSERT INTO produto (codigo,nome,lote) VALUES (46494, 'loja de conveniencia', '7yw34yr873-4387');
+INSERT INTO produto (codigo,nome,lote) VALUES (60554, 'peroxido de oxigenio', 'd9s0h2-548');
+INSERT INTO produto (codigo,nome,lote) VALUES (64199, 'cesio-137', 'cdi9767ws-099');
+INSERT INTO produto (codigo,nome,lote) VALUES (01380, 'persona 5', '12wdguiju765edf-111');
+INSERT INTO produto (codigo,nome,lote) VALUES (61727, 'sonhos', '43ue83j4ujtr-7833');
+INSERT INTO produto (codigo,nome,lote) VALUES (20914, 'esperancas', 'oijhgfgj8h798-475');
+INSERT INTO produto (codigo,nome,lote) VALUES (70527, 'loja de conveniencia 2', 'p2iki3jm4ni2j-8463');
+INSERT INTO produto (codigo,nome,lote) VALUES (67948, 'carteira_bitcoin.wcry', '5xc4787v6cx7c-1337');
+INSERT INTO produto (codigo,nome,lote) VALUES (64868, 'maquiagem de palhaco', 'voucoringar-666');
+INSERT INTO produto (codigo,nome,lote) VALUES (48234, 'lista de produtos', 'dewd433f5-3284');
+INSERT INTO produto (codigo,nome,lote) VALUES (42589, '42.zip', '4443f5f3f3t6gfgh6-4923');
+INSERT INTO produto (codigo,nome,lote) VALUES (04379, 'sociedade', 're8g7re8gre8-2814');
+INSERT INTO produto (codigo,nome,lote) VALUES (13030, 'botao de auto-destruicao', '7e8rfyregfyref-2314');
+INSERT INTO produto (codigo,nome,lote) VALUES (64697, 'atendente', 'x8eyw7dhuewd-9546');
+INSERT INTO produto (codigo,nome,lote) VALUES (04000, 'meta-referencia', 'xdwded87wged-43');
+INSERT INTO produto (codigo,nome,lote) VALUES (50273, '50273', 'wuee87dg8-000');
+INSERT INTO produto (codigo,nome,lote) VALUES (93280, 'prova de que P=NP', '985ueiwr3trwq65wt-2');
+-- fornecedores
+-- dependentes
+-- produtos fornecidos
+-- reabastecimentos
+-- vendas
