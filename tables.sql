172bfa9dc615198a63dda3012880f50c2ee3621f
Renato Alpes <renatinhoas@hotmail.com>
2023-07-20 18:38:32 +0000

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -18,6 +18,7 @@ CREATE TABLE pessoa (
   cep VARCHAR2(8) NOT NULL,
   idade NUMBER NOT NULL
   CONSTRAINT pessoa_pk PRIMARY KEY (cpf)
+  CONSTRAINT pessoa_ck CHECK (cep LIKE '_____-___')
 );
 
 CREATE TABLE telefone (
@@ -36,28 +37,50 @@ CREATE TABLE cliente (
 );
 
 CREATE TABLE produto (
-  
+  codigo NUMBER NOT NULL,
+  nome VARCHAR2(30) NOT NULL,
+  lote VARCHAR2(30) NOT NULL,
+  CONSTRAINT produto_pk PRIMARY KEY (codigo)
 );
 
 CREATE TABLE fornecedor (
-  
+  cnpj VARCHAR2(14) NOT NULL,
+  nome_da_empresa(50) NOT NULL,
+  CONSTRAINT fornecedor_pk PRIMARY KEY (cnpj)
 );
 
 CREATE TABLE dependente (
-  nome_completo VARCHAR2(50),
-  cpf_empr VARCHAR2(11),
+  nome_completo VARCHAR2(50) NOT NULL,
+  cpf_empr VARCHAR2(11) NOT NULL,
   CONSTRAINT dependente_pk PRIMARY KEY (cpf_empr,nome_completo),
   CONSTRAINT dependente_fk FOREIGN KEY (cpf_empr) REFERENCES funcionario(cpf)
 );
 
 CREATE TABLE produtoFornecido (
-  
+  cnpj VARCHAR2(14) NOT NULL,
+  codigo NUMBER NOT NULL,
+  CONSTRAINT produtoFornecido_pk PRIMARY KEY (cnpj,codigo),
+  CONSTRAINT produtoFornecido_fornecedor_fk FOREIGN KEY (cnpj) REFERENCES fornecedor(cnpj),
+  CONSTRAINT produtoFornecido_produto_fk FOREIGN KEY (codigo) REFERENCES produto(codigo)
 );
 
 CREATE TABLE reabastece (
-  
+  cnpj VARCHAR2(14),
+  codigo NUMBER,
+  cpf VARCHAR2(11),
+  CONSTRAINT reabastece_pk PRIMARY KEY (cnpj,codigo,cpf),
+  CONSTRAINT reabastece_fornecedor_fk (cnpj) REFERENCES fornecedor(cnpj),
+  CONSTRAINT reabastece_produto_fk FOREIGN KEY (codigo) REFERENCES produto(codigo),
+  CONSTRAINT reabastece_funcionario_fk FOREIGN KEY (cpf) REFERENCES funcionario(cpf)
 );
 
 CREATE TABLE vende (
-  
+  cpf VARCHAR2(11) NOT NULL,
+  cpf_cliente VARCHAR2(11) NOT NULL,
+  codigo NUMBER NOT NULL,
+  data_e_hora DATE NOT NULL,
+  CONSTRAINT vende_pk PRIMARY KEY (codigo,data_e_hora),
+  CONSTRAINT vende_funcionario_fk FOREIGN KEY (cpf) REFERENCES funcionario(cpf),
+  CONSTRAINT vende_cliente_fk FOREIGN KEY (cpf_cliente) REFERENCES cliente(cpf),
+  CONSTRAINT vende_produto_fk FOREIGN KEY (codigo) REFERENCES produto(codigo)
 );
