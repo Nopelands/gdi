203520157e505b1039e9ed6de212fa9ef3858047
Renato Alpes <renatinhoas@hotmail.com>
2023-07-25 20:58:00 +0000

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -30,23 +30,65 @@ SELECT * FROM dependente
 WHERE cpf_empr = '29909148031';
 
 -- Between
+
+SELECT cpf FROM cliente 
+WHERE pontos BETWEEN 1 AND 100;
+
 -- In
+
+SELECT codigo FROM produto
+WHERE nome IN ('sonhos','esperancas');
+
 -- Like
+
+SELECT codigo FROM produto
+WHERE nome LIKE 'loja%';
+
 -- Is Null || Is Not Null
+
+SELECT codigo FROM produto
+WHERE lote IS NOT NULL;
+
 -- Inner Join
 -- Max
+
+SELECT MAX (idade) FROM pessoa;
+  
 -- Min
+
+SELECT MIN (pontos) FROM cliente;
+
 -- Avg
+
+SELECT AVG (idade) FROM pessoa;
+
 -- Count
+
+SELECT COUNT (*) FROM produtoFornecido;
+
 -- Left || Right || Full Outer Join
 -- Subconsulta com Operador Relacional
 -- Subconsulta com In
 -- Subconsulta com Any
 -- Subconsulta com All
 -- Order By
+
+SELECT nome FROM pessoa
+ORDER BY idade;
+
 -- Group By
+
+SELECT cnpj, COUNT(*) FROM produtoFornecido
+GROUP BY cnpj;
+
 -- Having
+
+SELECT cnpj FROM produtoFornecido
+GROUP BY cnpj HAVING COUNT(*) > 1;
+
 -- Union || Intersect Minus
+
+
 -- Create View
 -- Grant / Revoke
 
