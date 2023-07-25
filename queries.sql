9185fc600a738ba0a11bc5bef937bc4bb1ef0637
Fabrício Aranha <fabricioaranha1@gmail.com>
2023-07-25 13:52:35 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,9 +1,34 @@
 -- Alter Table
+
+ALTER TABLE pessoa 
+ADD (sexo CHAR);
+
 -- Create Index
+
+
+
 -- Insert Into
+
+INSERT INTO funcionario (cpf,cargo,cpf_supervisor) 
+VALUES ('00698199049', 'atendente', '82322846090');
+
+
 -- Update
+
+UPDATE funcionario
+SET cargo = 'supervisor'
+WHERE cpf = '29909148031';
+
 -- Delete
+
+DELETE funcionario
+WHERE cpf = '00698199049';
+
 -- Select-From-Where
+
+SELECT * FROM dependente 
+WHERE cpf_empr = '29909148031';
+
 -- Between
 -- In
 -- Like
