c8987f7e33cb60df7fa2edc971aef99d7a4c9280
Fabrício Aranha <fabricioaranha1@gmail.com>
2023-09-11 12:09:07 -0300

    Adicionando checklist

--- MongoDB/queries.js
+++ MongoDB/queries.js
@@ -7,5 +7,9 @@ use("agencia");
 db.roteiro.find({paisId: db.pais.findOne({nome: "China"})._id}, 
 {_id: 0, nome: 1, descricao: 1}).pretty();
 
-//
+// Retorna todas as atrações em que o preço de entrada é maior ou igual a 40
+
+db.atracao.find({precoEntrada: {$gte: 40.00}}).pretty();
+
+// 
 
